4c3526756dd7475ccd1a83d911a7227e7727ee6c
Yu-kiYamano <yuki.yamano@topgate.co.jp>
2020-06-17 17:43:12 +0900

    connect view

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.14
 require (
 	github.com/flosch/pongo2 v0.0.0-20200529170236-5abacdfa4915
 	github.com/gin-gonic/gin v1.6.3
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.1.16
 )
--- main.go
+++ main.go
@@ -1,41 +1,25 @@
 package main
 
-<<<<<<< HEAD
 import (
 	"net/http"
 
-	"github.com/gin-gonic/gin"
-)
-
-func main() {
-	router := gin.Default()
-	router.GET("/", Index)
-	router.Run(":8080")
-}
-
-func Index(c *gin.Context) {
-	c.String(http.StatusOK, "hello world")
-}
-=======
-import(
-	"net/http"
-	"time"
-
 	"github.com/flosch/pongo2"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-const tmplPath = "src/template"
+const tmplPath = "src/view"
 
 var e = createMux()
 
-func main(){
-	e.GET("/",articleIndex)
-	e.Logger.Fatal(e.Start("8080"))
+func main() {
+	e.GET("/", Index)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
 
-func createMux() *echo.Echo{
+func createMux() *echo.Echo {
 	e := echo.New()
 
 	e.Use(middleware.Recover())
@@ -45,17 +29,21 @@ func createMux() *echo.Echo{
 	return e
 }
 
-func articleIndex(c echo.Context) error{
+func Index(c echo.Context) error {
 	data := map[string]interface{}{
-		"Message": "Hello,World",
-		"Now": time.Now(),
+		"Message": "test",
 	}
-	return render(c echo.Context,file string,data map[string]interface{}) error{
-		b, err := htmlBlob(file,data)
-		if err != nil{
-			return c.NoContent(http.StatusInternalServerError)
-		}
-		return c.HTMLBlob(http.statusOK,b)
-	}
+	return render(c, "src/views/index.html", data)
 }
->>>>>>> origin/backend
+
+func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
+	return pongo2.Must(pongo2.FromCache(file)).ExecuteBytes(data)
+}
+
+func render(c echo.Context, file string, data map[string]interface{}) error {
+	b, err := htmlBlob(file, data)
+	if err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	return c.HTMLBlob(http.StatusOK, b)
+}
