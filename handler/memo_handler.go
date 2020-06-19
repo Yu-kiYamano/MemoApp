c20c0337c5efb4790d28a5ff6c87615874661e7c
Yu-kiYamano <yuki.yamano@topgate.co.jp>
2020-06-19 15:25:54 +0900

    fix

--- handler/memo_handler.go
+++ handler/memo_handler.go
@@ -12,75 +12,60 @@ import (
 )
 
 func MemoIndex(c echo.Context) error {
-	// リポジトリの処理を呼び出して記事の一覧データを取得します。
+
 	memos, err := repository.MemoListByCursor(0)
 
-	// エラーが発生した場合
 	if err != nil {
-		// エラー内容をサーバーのログに出力します。
+
 		c.Logger().Error(err.Error())
 
-		// クライアントにステータスコード 500 でレスポンスを返します。
 		return c.NoContent(http.StatusInternalServerError)
 	}
 
-	// テンプレートに渡すデータを map に格納します。
 	data := map[string]interface{}{
 		"Memos": memos,
 	}
 
-	// テンプレートファイルとデータを指定して HTML を生成し、クライアントに返却します
 	return render(c, "src/views/index.html", data)
 }
 
-// ArticleCreateOutput ...
 type MemoCreateOutput struct {
 	Memo             *model.Memo
 	Message          string
 	ValidationErrors []string
 }
 
-// ArticleCreate ...
 func MemoCreate(c echo.Context) error {
-	// 送信されてくるフォームの内容を格納する構造体を宣言します。
 	var memo model.Memo
 
-	// レスポンスとして返却する構造体を宣言します。
 	var out MemoCreateOutput
 
-	// フォームの内容を構造体に埋め込みます。
 	if err := c.Bind(&memo); err != nil {
-		// エラーの内容をサーバーのログに出力します。
+
 		c.Logger().Error(err.Error())
 
-		// リクエストの解釈に失敗した場合は 400 エラーを返却します。
 		return c.JSON(http.StatusBadRequest, out)
 
 	}
 
-	// repository を呼び出して保存処理を実行します。
 	res, err := repository.MemoCreate(&memo)
 	if err != nil {
-		// エラーの内容をサーバーのログに出力します。
+
 		c.Logger().Error(err.Error())
 
-		// サーバー内の処理でエラーが発生した場合は 500 エラーを返却します。
 		return c.JSON(http.StatusInternalServerError, out)
 	}
 
-	// SQL 実行結果から作成されたレコードの ID を取得します。
 	id, _ := res.LastInsertId()
 
-	// 構造体に ID をセットします。
 	memo.ID = int(id)
 
-	// レスポンスの構造体に保存した記事のデータを格納します。
 	out.Memo = &memo
 
-	// 処理成功時はステータスコード 200 でレスポンスを返却します。
 	return c.JSON(http.StatusOK, out)
 }
 
+//削除機能
 func MemoDelete(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
 
--- repository/memo_repository.go
+++ repository/memo_repository.go
@@ -8,50 +8,39 @@ import (
 )
 
 func MemoCreate(memo *model.Memo) (sql.Result, error) {
-	// クエリ文字列を生成します。
+
 	query := `INSERT INTO memos (memo)
 	VALUES (:memo);`
 
-	// トランザクションを開始します。
 	tx := db.MustBegin()
 
-	// クエリ文字列と構造体を引数に渡して SQL を実行します。
-	// クエリ文字列内の「:title」「:body」「:created」「:updated」は構造体の値で置換されます。
-	// 構造体タグで指定してあるフィールドが対象となります。（`db:"title"` など）
 	res, err := tx.NamedExec(query, memo)
 	if err != nil {
-		// エラーが発生した場合はロールバックします。
+
 		tx.Rollback()
 
-		// エラー内容を返却します。
 		return nil, err
 	}
 
-	// SQL の実行に成功した場合はコミットします。
 	tx.Commit()
 
-	// SQL の実行結果を返却します。
 	return res, nil
 }
 
 func MemoListByCursor(cursor int) ([]*model.Memo, error) {
-	// 引数で渡されたカーソルの値が 0 以下の場合は、代わりに int 型の最大値で置き換えます。
+
 	if cursor <= 0 {
 		cursor = math.MaxInt32
 	}
 
-	// ID の降順に記事データを 10 件取得するクエリ文字列を生成します。
 	query := `SELECT *
 	FROM memos
 	WHERE id < ?
 	ORDER BY id desc
 	LIMIT 10`
 
-	// クエリ結果を格納するスライスを初期化します。
-	// 10 件取得すると決まっているため、サイズとキャパシティを指定しています。
 	memos := make([]*model.Memo, 0, 10)
 
-	// クエリ結果を格納する変数、クエリ文字列、パラメータを指定してクエリを実行します。
 	if err := db.Select(&memos, query, cursor); err != nil {
 		return nil, err
 	}
